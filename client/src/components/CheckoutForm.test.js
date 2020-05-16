790d983e59e0a95dcd09bd97db7f3611d519060c
JOliver23 <59593045+JOliver23@users.noreply.github.com>
2020-05-15 20:01:19 -0700

    tests built

--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -1,9 +1,19 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("form header renders", () => {});
+test("form header renders", () => {
+    const {getByText} = render(<CheckoutForm />);
+    const header = getByText(/checkout form/i);
+    expect(header).toBeInTheDocument;
+});
 
-test("form shows success message on submit with form details", () => {});
+test("form shows success message on submit with form details", () => {
+    const {getByTestId, getByText} = render(<CheckoutForm />);
+    const chckbtn = getByText(/checkout/i);
+    const confirmation = getByTestId("successMessage");
+    fireEvent.click(chckbtn);
+    expect(confirmation).toBeInTheDocument;
+});
