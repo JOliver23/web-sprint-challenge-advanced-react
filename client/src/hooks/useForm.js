8a3a42659b91755472d553a2d35aca35eea1e54a
JOliver23 <59593045+JOliver23@users.noreply.github.com>
2020-05-15 19:46:34 -0700

    custom hook built, not sure what its supposed to do regarding stateful logic

--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -1,1 +1,11 @@
 // write your custom hook here to control your checkout form
+import { useEffect } from 'react';
+
+export const useForm = (initialState, key) => {
+    const [value, setValue] = useState(initialState, key);
+    useEffect(() => {
+        if (value) {
+            document.querySelector("body")
+        }
+    }, [value])
+}
